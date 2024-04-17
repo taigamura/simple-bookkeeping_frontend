d40050bbf9a5136790a95056a8d652a8624b48dd
Kimura <taiga.kimura@sap.com>
2024-04-17 13:17:35 +0900

    add axios

--- src/calendar-days.js
+++ src/calendar-days.js
@@ -1,43 +1,62 @@
-function CalendarDays(props) {
-    let firstDayOfMonth = new Date(props.day.getFullYear(), props.day.getMonth(), 1);
-    let weekdayOfFirstDay = firstDayOfMonth.getDay();
-    let currentDays = [];
+import axios from 'axios';
 
-    for (let day = 0; day < 42; day++) {
-        if (day === 0 && weekdayOfFirstDay === 0) {
-          firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
-        } else if (day === 0) {
-          firstDayOfMonth.setDate(firstDayOfMonth.getDate() + (day - weekdayOfFirstDay));
-        } else {
-          firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
-        }
-    
-        let calendarDay = {
-          currentMonth: (firstDayOfMonth.getMonth() === props.day.getMonth()),
-          date: (new Date(firstDayOfMonth)),
-          month: firstDayOfMonth.getMonth(),
-          number: firstDayOfMonth.getDate(),
-          selected: (firstDayOfMonth.toDateString() === props.day.toDateString()),
-          year: firstDayOfMonth.getFullYear()
-        }
-    
-        currentDays.push(calendarDay);
-      }
-      
-    return (
-    <div className="table-content">
-        {
-        currentDays.map((day) => {
-            return (
-            <div className={"calendar-day" + (day.currentMonth ? " current" : "") + (day.selected ? " selected" : "")}
-                    onClick={() => props.changeCurrentDay(day)}>
-                <p>{day.number}</p>
-            </div>
-            )
-        })
-        }
-    </div>
-    )
+function CalendarDays(props) {
+  const firstDayOfMonth = new Date(props.day.getFullYear(), props.day.getMonth(), 1);
+  const weekdayOfFirstDay = firstDayOfMonth.getDay();
+  let currentDates = [];
+
+  for (let day = 0; day < 42; day++) {
+    if (day === 0 && weekdayOfFirstDay === 0) {
+      firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
+    } else if (day === 0) {
+      firstDayOfMonth.setDate(firstDayOfMonth.getDate() + (day - weekdayOfFirstDay));
+    } else {
+      firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
+    }
+
+    // retrieve from server
+    state = {
+      data: []
+    }
+
+    let retrievedAmount = axios.get('http://localhost:3000/record?username=admin')
+      .then(function (res) {
+        // console.log(res.data[0].amount);
+        const data = res.data[0]
+        this.setState({data});
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+
+    // set fields
+    let calendarDay = {
+      currentMonth: (firstDayOfMonth.getMonth() === props.day.getMonth()),
+      date: (new Date(firstDayOfMonth)),
+      month: firstDayOfMonth.getMonth(),
+      number: firstDayOfMonth.getDate(),
+      selected: (firstDayOfMonth.toDateString() === props.day.toDateString()),
+      year: firstDayOfMonth.getFullYear(),
+      amount: retrievedAmount
+    }
+
+    currentDates.push(calendarDay);
   }
-  
-  export default CalendarDays;
+
+  return (
+    <div className="table-content">
+      {
+        currentDates.map((day) => {
+          return (
+            <div className={"calendar-day" + (day.currentMonth ? " current" : "") + (day.selected ? " selected" : "")}
+                onClick={() => props.changeCurrentDay(day)}>
+              <p>{day.number}<br></br>{day.amount}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default CalendarDays;
--- src/calendar.js
+++ src/calendar.js
@@ -7,23 +7,54 @@ export default class Calendar extends Component {
     super();
 
     this.weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    this.months = ['January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'];
+    this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     this.state = {
-      currentDay: new Date()
+      currentDate: new Date()
     }
   }
 
   changeCurrentDay = (day) => {
-    this.setState({ currentDay: new Date(day.year, day.month, day.number) });
+    this.setState({ currentDate: new Date(day.year, day.month, day.number) });
+  }
+
+  nextMonth = () => {
+    this.setState({ currentDate: new Date(this.state.currentDate.setMonth(this.state.currentDate.getMonth() + 1)) });
+  }
+
+  prevMonth = () => {
+    this.setState({ currentDate: new Date(this.state.currentDate.setMonth(this.state.currentDate.getMonth() - 1)) });
+  }
+
+  returnCurrent = () => {
+    this.setState({ currentDate: new Date() });
   }
 
   render() {
     return (
       <div className="calendar">
         <div className="calendar-header">
-          <h2>{this.months[this.state.currentDay.getMonth()]} {this.state.currentDay.getFullYear()}</h2>
+          <div className="title">
+            <h2>{this.months[this.state.currentDate.getMonth()]} {this.state.currentDate.getFullYear()}</h2>
+          </div>
+          <div className="tools">
+            <button onClick={this.prevMonth}>
+              <span className="material-icons">
+                arrow_back
+                </span>
+            </button>
+            <p>{this.months[this.state.currentDate.getMonth()].substring(0, 3)} {this.state.currentDate.getDate()}</p>
+            <button onClick={this.nextMonth}>
+              <span className="material-icons">
+                arrow_forward
+                </span>
+            </button>
+            <button onClick={this.returnCurrent}>
+              <span className="material-icons">
+                return_current
+                </span>
+            </button>
+          </div>
         </div>
         <div className="calendar-body">
           <div className="table-header">
@@ -33,7 +64,7 @@ export default class Calendar extends Component {
               })
             }
           </div>
-          <CalendarDays day={this.state.currentDay} changeCurrentDay={this.changeCurrentDay} />
+          <CalendarDays day={this.state.currentDate} changeCurrentDay={this.changeCurrentDay} />
         </div>
       </div>
     )
